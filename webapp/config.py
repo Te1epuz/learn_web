155c69f99c6ad707511d55eb446371b9cf77146c
Alexander M <alexander_martinovich@msn.com>
2019-06-12 17:12:31 +0300

    major upd

---
+++ webapp/config.py
@@ -0,0 +1,5 @@
+WEATHER_DEFAULT_CITY = 'Moscow,Russia'
+WEATHER_API_KEY = '9e8229dfc5f144c4b3d204731193105'
+WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+
+# потом добавить в гитигнор
