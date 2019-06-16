33e54ec7d5a33fbeff64686ac1bc2e1516ed91cc
Alexander M <alexander_martinovich@msn.com>
2019-06-17 01:28:41 +0300

    добавлена модель логина

--- webapp/config.py
+++ webapp/config.py
@@ -7,4 +7,4 @@ WEATHER_API_KEY = '9e8229dfc5f144c4b3d204731193105'
 WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
 
-# потом добавить в гитигнор
+SECRET_KEY = 'urpwoeriryquiweqwepqwe'
